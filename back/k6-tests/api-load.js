146d284c9d9dfc9cf0337586b898b2ab56efbc6f
gaellegoyon <gaellegoyon.pro@gmail.com>
2025-07-18 10:24:29 +0200

    fix: test

--- back/k6-tests/api-load.js
+++ back/k6-tests/api-load.js
@@ -2,6 +2,6 @@ import http from 'k6/http';
 import { check } from 'k6';
 
 export default function () {
-  const res = http.get('https://ton-backend-render.io/api/health');
-  check(res, { 'status is 200': (r) => r.status === 200 });
+    const res = http.get('https://plany-app.onrender.com/');
+    check(res, { 'status is 200': (r) => r.status === 200 });
 }
